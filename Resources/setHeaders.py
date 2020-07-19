2a78d55111332d0adb218c2b57c38074e5cba2e5
Lauren Gordon-Fahn <lgordon-fahn@MKE-LGFahn.attlocal.net>
2020-07-19 13:11:56 -0500

    Fixed headers

--- Resources/setHeaders.py
+++ Resources/setHeaders.py
@@ -1,17 +1,17 @@
 import os
 import boto3
-from response import Response
-from ssm import getSSM
+from Resources.response import Response
+from Resources.ssm import getSSM
 
 class SetHeaders:
     def __init__(self):
-        self.APIKEY = getSSM.response
+        self.APIKEY = getSSM('YELP_API_KEY')
 
     def createHeader(self):
         '''Creates the needed headers to call the API Gateway'''
         if self.APIKEY.success:
-            headers = {'content-type' : "application/json", 'Bearer API_KEY ': self.APIKEY.response}
-            return headers
-        return {'content-type' : "application/json"}
+            headers = {'content-type' : "application/json", 'Authorization':'Bearer ' + self.APIKEY.response}
+            return Response(True, headers, "Successfully set headers")
+        return Response(False, None, "Failed to set retrieve API Key to set headers")
 
     
